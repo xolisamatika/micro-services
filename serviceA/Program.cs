0e247fad0f581fb5390d2cc314af78a0965af3df
Xolisa Matika <xolisa.matika@bettech.com>
2018-04-25 16:10:25 +0200

    First commit

---
+++ serviceA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace serviceA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(value: "Hello, please enter your name : ");
+            String name = Console.ReadLine();
+            
+            String message = $"Hello my name is, {name}";
+            send(message: message);
+
+        }
+        private static void send(String message)
+        {
+            new ConnectionFactory().HostName = "localhost";
+
+            using (var connection = new ConnectionFactory().CreateConnection()) 
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "myQ",durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",  routingKey: "myQ", basicProperties: null, body: body);
+                    Console.WriteLine("Message sent!");
+                }
+            }
+        }
+    }
+}
---
+++ serviceB/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace serviceB
+{
+    class Program
+    {
+        static void Main(string[] args) => listen();
+
+        public static void listen()
+        {
+            Console.WriteLine("Listening...");
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            using (var connection = factory.CreateConnection()){
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "myQ", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = System.Text.Encoding.UTF8.GetString(body);
+                        var name = message.Split(", ")[1];
+                        if (string.IsNullOrWhiteSpace(name))
+                        {
+                             throw new ArgumentException("name", nameof(name));
+                        }
+                        Console.WriteLine("Hello {0}, I am your father!", name);
+                    };
+                    channel.BasicConsume(queue: "myQ", autoAck: true, consumer: consumer);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
